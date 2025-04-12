a79b64431f59b4eb35d8706bf33a2210d655750b
Ma1n <kskiller2020@gmail.com>
2025-04-12 20:58:21 +0300

    Initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { StartScreenComponent } from './start-screen/start-screen.component';
+
+export const routes: Routes = [{
+    path:'Home', component:StartScreenComponent
+}];
---
+++ src/app/start-screen/start-screen.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-start-screen',
+  imports: [],
+  standalone:true,
+  templateUrl: './start-screen.component.html',
+  styleUrl: './start-screen.component.css'
+})
+export class StartScreenComponent {
+
+}
