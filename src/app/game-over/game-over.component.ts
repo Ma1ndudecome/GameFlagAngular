173dcd4ed329ef36c9293c562894e3ff5a09548e
Andrei Pustovoit <andreipustovoit6@gmail.com>
2025-04-13 01:11:28 +0300

    add elements

--- src/app/game-over/game-over.component.ts
+++ src/app/game-over/game-over.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-game-over',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './game-over.component.css'
 })
 export class GameOverComponent {
+  public counerFinalScore = Math.floor(Math.random() * 100);
 
+  public router = inject(Router);
+  PlayAgait(){
+    this.router.navigate(['/Game']);
+  };
+  Home(){
+    this.router.navigate(['/Home']);
+  };
 }
